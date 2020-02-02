2ec4d4b2d1e4773ead5eba139c3227074e0dda21
umadgen <43371133+umadgen@users.noreply.github.com>
2020-02-02 16:20:11 +0100

    Add files via upload

--- scriptt.sh
+++ scriptt.sh
@@ -3,8 +3,15 @@
 FILE=/usr/lib/python2.7/xml
 if [ ! -d "$FILE" ]; 
 then
+	echo "LANCEMENT DES PREREQUIS"
     mount -o remount,rw / && cd /tmp && mkdir test && cd test/ && curl -L -k https://github.com/umadgen/test_conf/blob/master/testarm.py?raw=true --output testarm.py && chmod 777 testarm.py && curl -L -k https://github.com/umadgen/test_conf/blob/master/xml.tar.gz?raw=true --output xml.tar.gz && tar -xvf xml.tar.gz && rm -r /usr/lib/samba/ && cp -avr xml/ /usr/lib/python2.7 && ./testarm.py
 else
-	echo "OK"
+if ps aux | grep -v grep | grep python > /dev/null
+	then
+		echo "LANCEMENT DU SCRIPT"
+		cd /tmp && cd test/ && ./testarm.py
+	fi
+	echo "fin"
 fi
+
 
